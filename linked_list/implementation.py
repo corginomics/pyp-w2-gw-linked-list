be9219386befc28f5ec991836909f51d5867fd3f
corginomics <anvle@ucdavis.edu>
2016-07-15 21:34:20 +0000

    Finish another case for pop

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -88,19 +88,52 @@ class LinkedList(AbstractLinkedList):
         #pop_node = self.start
         # when index == None
         
-        if index is None:   # user doesn't input an index
+        #popping the end
+        if index is None or (len(self) - 1 == index):
             for node in self:
                 if node.next == self.end:   # if node is second-to-last-node
                     endValue = self.end.elem  # set endValue to last node's elem
                     self.end = node # change self.end to second to last node
                     self.end.next = None #change self.next to None
-            
                     return endValue
-                    
-                    
-
+        if index == 0:
+            importantValue = self.start.elem
+            nodeAhead = self.start.next
+            self.start = nodeAhead
+            return importantValue
+            
+        #if index == self.pop(index):
+        # user inputs 2. index is 2
+        # if 2 ==
+        # my understanding at this point is vague: 
+        
+        counter = 0
+        for node in self:
+            previousNode = node
+            node = node.next
+            nextNode = node.next
+            
+            if (counter == index - 1):
+                importantValue = node.elem
+                previousNode.next = nextNode
+                return importantValue
+            
+            counter += 1
+            
+        
+        #if we're po    
+        
+        #############dev below
+        # trying to build the pop thingey
+        #else:
+        # for node in self:
+        #     if index == self.start:
+        #         userValue = self.start.elem
+        #     return userValue
                 
-        # # user does input an index
+        
+        
+        # user does input an index
         # else:
         #     counter = 0
         #     for node in self:
@@ -110,14 +143,8 @@ class LinkedList(AbstractLinkedList):
                 
         #         if counter == index:
         #             #connect the 2 nodes here
-                    
         #             previousNode.next = nextNode
         #             return node.elem
                     
         #         counter += 1
-    
-            
-        
-            
-        
-        
+
